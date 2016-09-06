b6ef36b8ee5a169b150c6a14145e467ac5824b65
fina-death <finaldeath0953@gmail.com>
2016-09-06 16:10:16 +0800

    unity

---
+++ Unity/Lesson2/New Unity Project/Assets/Ball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().velocity = new Vector3(-8.0f, 8.0f, 0.0f);
+    }
+	
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+    private void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag == "cube")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity/Lesson2/New Unity Project/Assets/PLAY.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PLAY : MonoBehaviour {
+    private float jump_speed = 10.0f;
+    public bool is_land = false;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (is_land == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                is_land = false;
+                this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.up * jump_speed;
+            }
+        }
+	}
+    private void OnCollisionEnter(Collision target)
+    {
+        if (target.gameObject.tag == "plane")
+        {
+            is_land = true;
+        }
+    }
+}
